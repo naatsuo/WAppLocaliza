ea317911ef3d14997f61b72f3811c209000e75dc
Japon <naatsuo@gmail.com>
2022-04-21 07:04:33 -0300

    inserted types cars 'fuel' and 'category'

---
+++ WAppLocaliza/Entities/Car.cs
@@ -0,0 +1,31 @@
+﻿namespace WAppLocaliza.Entities
+{
+    public enum FuelType
+    {
+        Gasoline,
+        Alcohol,
+        Diesel
+    }
+
+    public enum CategoryType
+    {
+        Basic,
+        Complete,
+        Deluxe
+    }
+
+    public class Car
+    {
+        public Guid Id { get; set; }
+        public string Plate { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Price { get; set; }
+        public string PriceHour { get; set; }
+        public FuelType Fuel { get; set; }
+        public string TrunkLimit { get; set; }
+        public CategoryType Category { get; set; } 
+        public DateTime CreatedAt { get; set; }
+    }
+}
+
