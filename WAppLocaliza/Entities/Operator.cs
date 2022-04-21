0f2584babf0858e20151de41cf8606d219a93f1b
Japon <naatsuo@gmail.com>
2022-04-21 06:54:20 -0300

    inserted new entity 'operator' and change entity 'user' inserted new informations

---
+++ WAppLocaliza/Entities/Operator.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WAppLocaliza.Entities
+{
+    public class Operator
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        [JsonIgnore]
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastAccessAt { get; set; }
+        public string[] Roles { get; set; }
+    }
+}
+
--- WAppLocaliza/Entities/User.cs
+++ WAppLocaliza/Entities/User.cs
@@ -5,9 +5,26 @@ namespace WAppLocaliza.Entities
     public class User
     {
         public Guid Id { get; set; }
-        public string Username { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string ZipCode { get; set; }
+        public string Address { get; set; }
+        public int Number { get; set; }
+        public string Complement { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public string Username { get; set; }//
         [JsonIgnore]
         public string Password { get; set; }
         public string[] Roles { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastAccessAt { get; set; }
     }
 }
+
